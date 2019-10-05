85c6814473a114bda1b8d5286f6d2bcb080ea562
Christos Tselepidas <ctselepidas@worth.systems>
2019-10-05 16:47:30 +0200

    [init-clone] removed some files, added some initialization

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# profile
+## Installing
+
+Installing the dependencies
+
+```bash
+npm i
+```
+
+## Start the dev server
+
+```bash
+npm start
+```
+
+## Clean the cache
+
+This removes the `.cache/` & `public/` folders
+
+```bash
+npm reset
+```
+
+## Credits to
+- Amazing illustrations by [Undraw.co](https://undraw.co)
---
+++ data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Christos Tselepidas',
+  author: 'Christos Tselepidas',
+  url: 'https://www.ct.tech',
+  legalName: 'Christos Tselepidas',
+  defaultDescription: 'I’m Christos and I’m a frontend focused software engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/ctselepidas',
+    github: 'https://github.com/yogainalift',
+    linkedin: 'https://www.linkedin.com/in/ctselepidas/',
+    youtube: 'https://www.youtube.com/user/yogainalift',
+    stackoverflow: 'undefined',
+    telegram: 't.me/yogainalift',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@yogainalift',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+  recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN',
+}
